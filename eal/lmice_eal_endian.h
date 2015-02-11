86bf0a3c1cd1f549276613d718e43558e158de4a
He Hao <hehaoslj@sina.com>
2015-02-11 18:56:04 +0800

    endian h

--- eal/lmice_eal_endian.h
+++ eal/lmice_eal_endian.h
@@ -1,18 +1,14 @@
 #ifndef LMICE_EAL_ENDIAN_H
 #define LMICE_EAL_ENDIAN_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-static inline int __attribute__((always_inline))
+static inline int forceinline
 eal_is_little_endian()
 {
     unsigned short ed = 0x0001;
     return (char)ed;
 }
 
-static inline int __attribute__((always_inline))
+static inline int forceinline
 eal_is_big_endian()
 {
     unsigned short ed = 0x0100;
@@ -20,8 +16,5 @@ eal_is_big_endian()
     return (char)ed;
 }
 
-#ifdef __cplusplus
-}
-#endif
 
 #endif /* LMICE_EAL_ENDIAN_H */
